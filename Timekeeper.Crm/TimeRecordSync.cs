3aecf35cfecf8a627f47bdf3276f9a181558da3e
danmannmann <dan@marsman.co.uk>
2015-11-01 12:44:53 +0000

    Got CRM connecting

--- Timekeeper.Crm/TimeRecordSync.cs
+++ Timekeeper.Crm/TimeRecordSync.cs
@@ -11,8 +11,13 @@ namespace Timekeeper.Crm
     {
         public TimeRecordSync(IEnumerable<TimeRecord> records)
         {
+            //var conn = new Microsoft.Xrm.Client.CrmConnection("Xrm");
+            //var creds = conn.ClientCredentials;
+
             Xrm.XrmServiceContext context = new Xrm.XrmServiceContext("Xrm");
-            var me = context.SystemUserSet.Where(x => x.LastName == "Mann").ToList();
+
+            var me = context.SystemUserSet.Where(x => x.LastName == "Mann").ToList().First();
+            var timeslots = me.user_new_timeslot;
         }
     }
 }
